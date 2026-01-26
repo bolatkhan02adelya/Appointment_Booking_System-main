096174908e21817646f2e48b8129ba2511b4d649
Adelya <adelyabolatkhan@gmail.com>
2026-01-26 13:31:51 +0600

    Initial commit

--- Appointment_Booking_System-main/Appointment_booking_system/src/main/java/config/DatabaseConfig.java
+++ Appointment_Booking_System-main/Appointment_booking_system/src/main/java/config/DatabaseConfig.java
@@ -5,17 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Appointments";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "02012008";
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/appointment_db";
-    private static final String USER = "postgres";      // твой username
-    private static final String PASSWORD = "password";  // твой пароль
 
-    public static Connection getConnection() throws SQLException {
+    public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
     public static void main(String[] args) {
-        try (Connection conn = getConnection()) {
+        DatabaseConfig config = new DatabaseConfig();
+
+        try (Connection conn = config.getConnection()) {
             if (conn != null) {
                 System.out.println("Соединение с базой данных успешно!");
             }
