f6d8433b539e667932b1a5152e9dfc6082697edb
didar <didarabss3@hotmail.com>
2026-02-01 21:07:08 +0500

    db name corrected

--- src/main/java/config/PostgresDB.java
+++ src/main/java/config/PostgresDB.java
@@ -7,10 +7,10 @@ public class PostgresDB implements IDB {
     @Override
     public Connection getConnection() {
 
-        String connectionUrl = "jdbc:postgresql://localhost:5432/AppointmentBookingSystem";
+        String connectionUrl = "jdbc:postgresql://localhost:5432/oop_assignment";
         try {
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(connectionUrl, "postgres", "0000");
+            return DriverManager.getConnection(connectionUrl, "postgres", "75812171");
         } catch (Exception e) {
             System.out.println("failed to connect to postgres: " + e.getMessage());
             return null;
