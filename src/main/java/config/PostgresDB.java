03dc796d4705f0aaeb0b0af55bed0ec4fc4ce05a
Adelya <adelyabolatkhan@gmail.com>
2026-02-08 18:26:49 +0600

    Adelya commit

--- src/main/java/config/PostgresDB.java
+++ src/main/java/config/PostgresDB.java
@@ -1,16 +1,32 @@
 package config;
+
 import config.interfaces.IDB;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class PostgresDB implements IDB {
+
+    private static PostgresDB instance;
+
+
+    private PostgresDB() {
+    }
+
+
+    public static PostgresDB getInstance() {
+        if (instance == null) {
+            instance = new PostgresDB();
+        }
+        return instance;
+    }
+
     @Override
     public Connection getConnection() {
-
-        String connectionUrl = "jdbc:postgresql://localhost:5432/AppointmentBookingSystem";
+        String connectionUrl = "jdbc:postgresql://localhost:5432/Appointment_booking_system";
         try {
             Class.forName("org.postgresql.Driver");
-            return DriverManager.getConnection(connectionUrl, "postgres", "0000");
+            return DriverManager.getConnection(connectionUrl, "postgres", "password");
         } catch (Exception e) {
             System.out.println("failed to connect to postgres: " + e.getMessage());
             return null;
